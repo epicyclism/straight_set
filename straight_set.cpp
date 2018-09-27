9da30116e4a44d8f956d64260cd4a20510ece481
Paul <paul@epicyclism.com>
2018-09-27 16:09:10 +0100

    Start to flesh out and include test

--- straight_set.cpp
+++ straight_set.cpp
@@ -4,7 +4,19 @@
 #include "straight_set.h"
 #include <iostream>
 
+template<typename C> void Print(C const& c)
+{
+	for (auto const& e : c)
+		std::cout << e << " ";
+	std::cout << "\n";
+}
+
 int main()
 {
+	epicyclism::straight_set<int> ssi{ 0, 1, 2, 3, 4, 5, 6, 7 };
+	Print(ssi);
+	epicyclism::straight_set<int> ssi2{ 0, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3 };
+	Print(ssi2);
+
 	return 0;
 }
--- straight_set.h
+++ straight_set.h
@@ -15,22 +15,55 @@ namespace epicyclism
 	{
 	private:
 		std::vector<Key> set_;
+		template<typename Ref> auto insert_(Ref t)
+		{
+			auto ip = find(t);
+			if (ip == end())
+			{
+				ip = set_.insert(end(), t);
+				return std::make_pair(ip, true);
+			}
+			return std::make_pair(ip, false);
+		}
+		template<typename ft> void insert1_(ft const& t)
+		{
+			auto ip = find(t);
+			if (ip == end())
+			{
+				set_.insert(end(), t);
+			}
+		}
 
 	public:
 		using key_type = Key;
-		using value_type = decltype(set_::value_type);
-		using size_type = decltype(set_::size_type);
-		using difference_type = decltype(set_::difference_type);
-		using key_equal = std::equal<Key>;
+		using value_type = Key;
+		using size_type = size_t;
+//		using key_equal = std::equal<Key>;
 		using reference = value_type & ;
 		using const_reference = const value_type&;
-		using iterator = decltype(set_::iterator);
-		using const_iterator = decltype(set_::const_iterator);
+		using iterator = typename std::vector<Key>::iterator;
+		using const_iterator = typename std::vector<Key>::const_iterator;
 
 		straight_set()
 		{
 			if constexpr (RESERVE > 0)
 				set_.reserve(RESERVE);
+		}
+		straight_set(straight_set const& other)
+		{
+			set_ = other.set_;
+		}
+		straight_set(std::initializer_list<value_type> iL)
+		{
+			insert(std::begin(iL), std::end(iL));
+		}
+		auto begin() const
+		{
+			return std::begin(set_);
+		}
+		auto end() const
+		{
+			return std::end(set_);
 		}
 		auto begin()
 		{
@@ -48,22 +81,6 @@ namespace epicyclism
 		{
 			return std::cend(set_);
 		}
-		auto rbegin()
-		{
-			return std::rbegin(set_);
-		}
-		auto rend()
-		{
-			return std::rend(set_);
-		}
-		auto rcbegin() const
-		{
-			return std::crbegin(set_);
-		}
-		auto crend() const
-		{
-			return std::crend(set_);
-		}
 		auto operator=(const straight_set& other)
 		{
 			set_ = other.set_;
@@ -77,7 +94,7 @@ namespace epicyclism
 		auto operator=(std::initializer_list<value_type> iL)
 		{
 			set_.clear();
-			std::copy(std::begin(iL), std::end(iL), std::back_inserter(set_));
+			insert1_(std::begin(iL), std::end(iL));
 			return *this;
 		}
 		auto find(Key& k)
@@ -88,27 +105,70 @@ namespace epicyclism
 		{
 			return std::find(cbegin(), cend(), k);
 		}
-		auto size() const
-		{
-			return set_.size();
-		}
 		auto empty() const
 		{
 			return set_.empty();
+		}
+		auto size() const
+		{
+			return set_.size();
 		}
 		auto max_size() const noexcept
 		{
 			return set_.max_size();
 		}
-		auto insert(T const& t)
+		void clear() noexcept
+		{
+			set_.clear();
+		}
+		auto insert(value_type const& t)
+		{
+			return insert_(t);
+		}
+		auto insert(value_type && t)
+		{
+			return insert_(t);
+		}
+		template<typename InIt> void insert(InIt first, InIt last)
+		{
+			while (first != last)
+			{
+				insert1_(*first);
+				++first;
+			}
+		}
+		template<typename InIt> void insert(std::initializer_list<value_type> iL)
+		{
+			insert(std::begin(iL), std::end(iL));
+		}
+		template<class... Args> auto emplace(Args&&... args)
 		{
 			auto ip = find(t);
 			if (ip == end())
 			{
-				ip = set_.insert(end(), t);
+				ip = set_.emplace_back(args);
 				return std::make_pair(ip, true);
 			}
 			return std::make_pair(ip, false);
 		}
+		auto erase(const_iterator pos)
+		{
+			return set_.erase(pos);
+		}
+		auto erase(const key_type& k)
+		{
+			auto ip = find(k);
+			if (ip != set_.end())
+			{
+				set_.erase(ip);
+				return 1;
+			}
+			return 0;
+		}
+		void swap(straight_set& other)
+		{
+			set_.swap(other.set_);
+		}
+
 	};
 }
